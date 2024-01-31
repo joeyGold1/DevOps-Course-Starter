387f1141aba3be6a8d3577afdfd5ef6b53b37b18
Joey Gold <joey.gold@softwire.com>
2024-01-31 12:13:12 +0000

    Stretch goals

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request
+from todo_app.data.session_items import add_item, get_item, get_items, remove_item, save_item
 
 from todo_app.flask_config import Config
 
@@ -8,4 +9,24 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = sorted(get_items(), key=lambda item: 1 if item['status'] == "Not Started" else 2)
+    return render_template('index.html', items=items)
+
+@app.route('/add', methods=["POST"])
+def add():
+    new_item = request.form.get('item_title')
+    add_item(new_item)
+    return redirect('/')
+
+@app.route('/toggle', methods=["POST"])
+def toggle_complete():
+    item_to_toggle = get_item(request.form.get('itemId'))
+    item_to_toggle['status'] = 'Complete' if item_to_toggle['status'] == 'Not Started' else 'Not Started'
+    save_item(item_to_toggle)
+    return redirect('/')
+
+@app.route('/remove', methods=['POST'])
+def remove():
+    itemId = request.form.get('itemId')
+    remove_item(itemId)
+    return redirect('/')
--- todo_app/data/session_items.py
+++ todo_app/data/session_items.py
@@ -67,3 +67,15 @@ def save_item(item):
     session['items'] = updated_items
 
     return item
+
+def remove_item(id):
+    """
+    Removes an existing item from the session. If no existing item matches the ID of the specified item, nothing is removed.
+
+    Args:
+        id: The ID of the item to remove.
+    """
+    existing_items = get_items()
+    updated_items = list(filter(lambda item: str(item['id']) != id, existing_items))
+
+    session['items'] = updated_items
